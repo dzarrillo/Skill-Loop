7219b54f5bf1fd9ff39e37a34e03810fc84dd8ff
Don Zarrillo Jr <donzarrillo@gmail.com>
2018-08-27 14:04:26 -0400

    Update Learn.js

--- client/src/pages/Learn/Learn.js
+++ client/src/pages/Learn/Learn.js
@@ -12,6 +12,9 @@ class Learn extends Component {
                         <p>The Learning Page</p>
                     </Col>
                 </Row>
+            <iframe src="https://jobscan.co/iframe?btncolor=1f57b5&utm_source=rutgersedubootcamp&utm_medium=iframe"width="800" height="650">
+                <p>Your browser does not support iframes.</p>
+                </iframe>
             </Container>
         );
     }
